608d28f8cbb10767091b13236f31bd16028bdfae
arnav <arnav.bholausa@gmail.com>
2024-01-11 01:11:32 -0500

    updating styles and routing

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles["container"]}>
+      <h1>FOOTER</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./Navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <div className={styles["container"]}>
+      <h1>NAVBAR</h1>
+    </div>
+  );
+};
+
+export default Navbar;
