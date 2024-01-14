1b383d3298aaf2b3adc9411c4ca8fe8d1e039e48
arnav <arnav.bholausa@gmail.com>
2024-01-13 19:58:23 -0500

    Adding socials

--- src/app/(public-pages)/team/page.jsx
+++ src/app/(public-pages)/team/page.jsx
@@ -13,18 +13,20 @@ const TeamPage = () => {
           name='Aarav Singh'
           text='Massa sapien faucibus et molestie ac. Tempus urna et pharetra pharetra massa massa ultricies. Eget magna fermentum iaculis eu non diam phasellus. In massa tempor nec feugiat nisl pretium. Eu turpis egestas pretium aenean pharetra. Suscipit adipiscing bibendum est ultricies integer quis auctor elit sed.'
           imageFirst
-          discord='https://www.google.com'
-          email='https://www.google.com'
+          discord='iambaby_yoda'
+          email='mailto:aaravsingh15.2020@gmail.com'
           instagram='https://www.instagram.com/young_yoda_2009/'
+          linkedin='https://www.linkedin.com/in/aarav-singh-273870222/'
         />
         <TeamMember
           name='Arnav Bhola'
           text='Massa sapien faucibus et molestie ac. Tempus urna et pharetra pharetra massa massa ultricies. Eget magna fermentum iaculis eu non diam phasellus. In massa tempor nec feugiat nisl pretium. Eu turpis egestas pretium aenean pharetra. Suscipit adipiscing bibendum est ultricies integer quis auctor elit sed.'
           imageFirst={false}
-          discord='https://www.google.com'
-          email='https://www.google.com'
+          discord='xyz.arnav.io'
+          email='mailto:arnav.bholausa@gmail.com'
           instagram='https://www.instagram.com/xyz.arnav.io/'
           github='https://github.com/Arnav-Bhola'
+          linkedin='https://www.linkedin.com/in/arnav-b-a1b05b26b/'
         />
       </div>
     </div>
