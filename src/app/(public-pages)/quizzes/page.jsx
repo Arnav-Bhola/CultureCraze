4cdc8c994a3d9dfb814955e73fbd95e1652be56f
arnav <arnav.bholausa@gmail.com>
2024-02-11 19:14:26 -0500

    mapping links

--- src/app/(public-pages)/quizzes/page.jsx
+++ src/app/(public-pages)/quizzes/page.jsx
@@ -17,40 +17,39 @@ const LandingPage = () => {
         <h1 className={styles["heading"]}>Try a quiz!</h1>
         <p className={styles["paragraph"]}>
           Welcome to CultureCraze, we&apos;re on a mission to bridge the gap caused by culture shock
-          and the challenges faced by travelers adjusting to new environments. Unfortunately, our
-          services aren&apos;t quite ready yet, but we&apos;re working tirelessly to bring you a
-          convenient solution.{" "}
+          and the challenges faced by travelers adjusting to new environments. We have created a
+          free product demonstration for you!
         </p>
       </div>
       <div className={styles["quizzes"]}>
         <Quiz
           heading='Geography'
           details='Dig into Polish rivers, mountains, weather, provinces and more!'
-          link=''
+          link='https://quizizz.com/admin/quiz/65c94acf111d93291a2c776d?searchLocale='
           image={geography}
         />
         <Quiz
           heading='Urban Cities'
           details='Learn about famous Polish cities and important locations around the country!'
-          link=''
+          link='https://quizizz.com/admin/quiz/65c8f30d9e3aef986786e92e?searchLocale='
           image={urbanCities}
         />
         <Quiz
           heading='Culture'
           details='Familiarize yourself on Polish etiquettes, customs and cultural norms!'
-          link=''
+          link='https://quizizz.com/admin/quiz/65c853838339c628796716c0?searchLocale='
           image={culture}
         />
         <Quiz
           heading='Food'
           details='Discover and Explore famous and delicious Polish meals and snacks!'
-          link=''
+          link='https://quizizz.com/admin/quiz/65c95eb8f277f67d81989ecf?searchLocale='
           image={food}
         />
         <Quiz
           heading='Language'
           details='Get introduced to the Polish language and common phrases'
-          link=''
+          link='https://quizizz.com/admin/quiz/65c8f30d9e3aef986786e92e?searchLocale='
           image={language}
         />
       </div>
